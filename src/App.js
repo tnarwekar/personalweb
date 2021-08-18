a59d4769a60b9474b8ed4d66f27deb3dec0eee28
tnarwekar <trishaprtn@gmail.com>
2021-08-18 11:34:35 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import myname from './mynamelogo.PNG'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={myname} className="App-logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
